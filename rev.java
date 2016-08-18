93970cba6b36ae175c84a43a16a7bee00cceb457
ManjuMuralidharan <manju1695@gmail.com>
2016-08-18 22:41:13 +0530

    Add files via upload

---
+++ rev.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class rev
+{
+public static void main(String[] args)
+{
+int n=1221;
+		int reverse=0;
+		int temp=n;
+		
+		while(n>0)
+		{
+			reverse=reverse*10;
+			reverse=reverse+n%10;
+			n=n/10;
+		}
+		
+		System.out.println("reversed number is" +reverse);
+		
+		if(temp==reverse)
+		{
+			System.out.println("palindrome");
+		}
+		else
+		{
+			System.out.println("not a palindrome");
+		}
+
+}
+}
